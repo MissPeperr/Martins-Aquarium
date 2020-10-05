cef4b3ac3235199f15d97a750a0a5245d80980e9
Madi Peper <madisonpeper@gmail.com>
2020-10-05 14:53:26 -0500

    Completed automate HTML chapter; All fish appear on DOM

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,20 +1,32 @@
 /**
- *  FishList which renders individual fish objects as HTML
+ *   FishList which renders individual fish objects as HTML
  */
-
 // TODO: Import `useFish` from the data provider module
-import { useFish } from './FishDataProvider.js'
+import { useFish } from "./FishDataProvider.js"
+import { Fish } from "./Fish.js"
 
 export const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
-    const contentElement = document.querySelector("insert selector here")
+    const contentElement = document.querySelector(".content")
     const fishes = useFish()
 
-    // Add to the existing HTML in the content element
+    // Generate all of the HTML for all of the fish
+    let fishHTMLRepresentations = ""
+    for (const fish of fishes) {
+        /*
+            Invoke the Fish component function
+            and pass the current fish object as an argument.
+            Each time, add the return value to the
+            fishHTMLRepresentations variable with `+=`
+        */
+        fishHTMLRepresentations += Fish(fish)
+    }
+
+    // Add a section, and all of the fish to the DOM
     contentElement.innerHTML += `
-        <article class="fishList">
-            All the fish go here!
+        <article class="fish__list">
+            ${fishHTMLRepresentations}
         </article>
     `
 }
