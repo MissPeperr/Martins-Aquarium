24b5773ab9cc894c1ab855e07f85b53ccc7d294c
Madi Peper <madisonpeper@gmail.com>
2020-10-05 13:03:29 -0500

    Completed chapter; Added every fish as an object in an array to the data provider;

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,36 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Bart",
+        length: "2 ft.",
+        species: "Nurse Shark",
+        food: "crustaceans",
+        harvestedAt: "Gulf of Mexico"
+    },
+    {
+        name: "Betty",
+        length: "2 ft.",
+        species: "Longtooth Barracuda",
+        food: "Squid",
+        harvestedAt: "Cumberland River"
+    },
+    {
+        name: "Nemo",
+        length: "2 in.",
+        species: "Clownfish",
+        food: "Squid",
+        harvestedAt: "Great Barrier Reef"
+    },
+    {
+        name: "Dory",
+        length: "4 in.",
+        species: "Blue Tang",
+        food: "Clownfish",
+        harvestedAt: "New Zealand Sea"
+    }
+]
